6291971cc1ba1464f8b71d1f1cf6ae5458a185bb
rocean <rocean74@gmail.com>
2024-11-06 23:19:57 +0200

    Some extra options and keymap for copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,3 +1,7 @@
 return {
     "github/copilot.vim",
+    config = function()
+        vim.g.copilot_no_tab_map = true
+        vim.api.nvim_set_keymap('i', '<A-CR>', 'copilot#Accept("<CR>")', { silent = true, expr = true, noremap = true })
+    end,
 }
