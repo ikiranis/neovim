e493d25d118639bdc052b7f4157ccaf82cfd1a85
rocean <rocean74@gmail.com>
2024-11-07 21:50:06 +0200

    Remap for dealing with word wrap

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,7 +1,6 @@
--- Display git changes in the sign column and stage/unstage hunks
+-- Adds git related signs to the gutter, as well as utilities for managing changes
 
 return {
-    -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
     opts = {
       -- See `:help gitsigns.txt`
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -69,4 +69,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   pattern = '*',
 })
 
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
