c5894477dd88c722e5aa7e3fb372d88e4d84f561
rocean <rocean74@gmail.com>
2024-11-12 10:39:17 +0200

    Set pathMappings

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -1,100 +1,101 @@
 return {
-    "mfussenegger/nvim-dap",
-    recommended = true,
-    desc = "Debugging support. Requires language specific adapters to be configured. (see lang extras)",
-    dependencies = {
-        "rcarriga/nvim-dap-ui",
-        "theHamsta/nvim-dap-virtual-text",
-        "nvim-neotest/nvim-nio",
-    },
-    config = function()
-        -- just want to make sure that we have dap and dapui
-        local has_dap, dap = pcall(require, "dap")
-        if not has_dap then
-            return
-        end
+	"mfussenegger/nvim-dap",
+	recommended = true,
+	desc = "Debugging support. Requires language specific adapters to be configured. (see lang extras)",
+	dependencies = {
+		"rcarriga/nvim-dap-ui",
+		"theHamsta/nvim-dap-virtual-text",
+		"nvim-neotest/nvim-nio",
+	},
+	config = function()
+		-- just want to make sure that we have dap and dapui
+		local has_dap, dap = pcall(require, "dap")
+		if not has_dap then
+			return
+		end
 
-        local has_dap_ui, dapui = pcall(require, "dapui")
-        if not has_dap_ui then
-            return
-        end
+		local has_dap_ui, dapui = pcall(require, "dapui")
+		if not has_dap_ui then
+			return
+		end
 
-        dap.adapters.php = {
-            type = 'executable',
-            command = 'node',
-            -- change this to where you build vscode-php-debug
-            args = { os.getenv("HOME") .. "/php-debug/out/phpDebug.js" },
-        }
+		dap.adapters.php = {
+			type = "executable",
+			command = "node",
+			-- change this to where you build vscode-php-debug
+			args = { os.getenv("HOME") .. "/php-debug/out/phpDebug.js" },
+		}
 
-        dap.configurations.php = {
-            -- to run php right from the editor
-            {
-                name = "run current script",
-                type = "php",
-                request = "launch",
-                port = 9003,
-                cwd = "${fileDirname}",
-                program = "${file}",
-                runtimeExecutable = "php"
-            },
-            -- to listen to any php call
-            {
-                name = "listen for Xdebug local",
-                type = "php",
-                request = "launch",
-                port = 9003,
-            },
-            -- to listen to php call in docker container
-            {
-                name = "listen for Xdebug docker",
-                type = "php",
-                request = "launch",
-                port = 9003,
-                -- this is where your file is in the container
-                pathMappings = {
-                    ["/var/www/html"] = "${workspaceFolder}"
-                }
-            }
-        }
+		dap.configurations.php = {
+			-- to run php right from the editor
+			{
+				name = "run current script",
+				type = "php",
+				request = "launch",
+				port = 9003,
+				cwd = "${fileDirname}",
+				program = "${file}",
+				runtimeExecutable = "php",
+			},
+			-- to listen to any php call
+			{
+				name = "listen for Xdebug local",
+				type = "php",
+				request = "launch",
+				port = 9003,
+			},
+			-- to listen to php call in docker container
+			{
+				name = "listen for Xdebug docker",
+				type = "php",
+				request = "launch",
+				port = 9003,
+				-- this is where your file is in the container
+				pathMappings = {
+					["/var/www/html/webapp"] = "${workspaceFolder}",
+					["/var/www/html"] = "${workspaceFolder}",
+				},
+			},
+		}
 
-        -- toggle the UI elements after certain events
-        dap.listeners.after.event_initialized["dapui_config"] = function()
-            dapui.open()
-        end
+		-- toggle the UI elements after certain events
+		dap.listeners.after.event_initialized["dapui_config"] = function()
+			dapui.open()
+		end
 
-        dap.listeners.before.event_terminated["dapui_config"] = function()
-            dapui.close()
-        end
+		dap.listeners.before.event_terminated["dapui_config"] = function()
+			dapui.close()
+		end
 
-        dap.listeners.before.event_exited["dapui_config"] = function()
-            dapui.close()
-        end
+		dap.listeners.before.event_exited["dapui_config"] = function()
+			dapui.close()
+		end
 
-        dapui.setup()
+		dapui.setup()
 
-        local function map(mode, lhs, rhs, opts)
-            local options = { noremap = true, silent = true }
-            if opts then
-                options = vim.tbl_extend('force', options, opts)
-            end
-            vim.keymap.set(mode, lhs, rhs, options)
-        end
+		local function map(mode, lhs, rhs, opts)
+			local options = { noremap = true, silent = true }
+			if opts then
+				options = vim.tbl_extend("force", options, opts)
+			end
+			vim.keymap.set(mode, lhs, rhs, options)
+		end
 
-        map("n", "<F5>", require "dap".continue, {})
-        map("n", "<F10>", require "dap".step_over, {})
-        map("n", "<F11>", require "dap".step_into, {})
-        map("n", "<F12>", require "dap".step_out, {})
-        map("n", "<leader>b", require "dap".toggle_breakpoint, {})
-        map("n", "<leader>du", ":lua require'dapui'.toggle()<cr>", {})
+		map("n", "<F5>", require("dap").continue, {})
+		map("n", "<F10>", require("dap").step_over, {})
+		map("n", "<F11>", require("dap").step_into, {})
+		map("n", "<F12>", require("dap").step_out, {})
+		map("n", "<leader>b", require("dap").toggle_breakpoint, {})
+		map("n", "<leader>du", ":lua require'dapui'.toggle()<cr>", {})
 
-        -- you'll want this because we don't want xdebug to start automatically everytime
-        function insert_xdebug()
-            local pos = vim.api.nvim_win_get_cursor(0)[2]
-            local line = vim.api.nvim_get_current_line()
-            local nline = line:sub(0, pos) .. 'xdebug_break();' .. line:sub(pos + 1)
-            vim.api.nvim_set_current_line(nline)
-        end
+		-- you'll want this because we don't want xdebug to start automatically everytime
+		function insert_xdebug()
+			local pos = vim.api.nvim_win_get_cursor(0)[2]
+			local line = vim.api.nvim_get_current_line()
+			local nline = line:sub(0, pos) .. "xdebug_break();" .. line:sub(pos + 1)
+			vim.api.nvim_set_current_line(nline)
+		end
 
-        map("n", "<leader>ds", "<cmd>lua insert_xdebug()<cr>")
-    end,
+		map("n", "<leader>ds", "<cmd>lua insert_xdebug()<cr>")
+	end,
 }
